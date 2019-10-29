f532f82876cdc505a7d2a4d3a70e4089714e58c8
Nick Killin <nicholaskillin@gmail.com>
2019-10-29 13:14:10 -0400

    add webhook to controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
     
-    def home
-    end
+  def home
+    HTTParty.get("https://maker.ifttt.com/trigger/portfolio_visited/with/key/#{Rails.application.credentials.ifttt_webhook_key}")
+  end
     
 end
