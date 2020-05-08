a290b82cf26c9aec8109543564ebc5af37e72087
Nick Killin <nicholaskillin@gmail.com>
2020-05-08 07:36:48 -0400

    stop the http calls on load

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ApplicationController
     
   def home
-    # HTTParty.get("https://maker.ifttt.com/trigger/portfolio_visited/with/key/#{Rails.application.credentials.ifttt_webhook_key}")
     repos = Github.repos.list user: 'nicholaskillin'
     @repos = repos.body
   end
